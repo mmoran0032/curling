b17c4e0c631488cb2fd5e52aad5b53b19d73f5d3
Mike Moran <mmoran0032@gmail.com>
2016-04-04 13:33:50 -0400

    ford world women's

--- README.md
+++ README.md
@@ -21,3 +21,26 @@ how I've gotten around that limitation, using a few websites.
     ```
 
 1.  Do a visual check that everything turns out right (should be fine)
+
+
+Data
+----
+
+From the individual games, I want to track (for each team involved) the
+following items:
+
+- if the team won (`won`)
+
+- team's final score (`score`)
+
+- final score differential (`score_diff`)
+
+- total ends played (`num_ends`)
+
+- number of scoring ends for the team (`scoring_ends`)
+
+- number of scoring ends while team had hammer (`scoring_hammer`)
+
+- number of blank ends (`blank_ends`)
+
+- number of blank ends while team had hammer (`blank_hammer`)
--- basicExtractCurling.py
+++ basicExtractCurling.py
@@ -1,97 +1,45 @@
 #!/usr/bin/env python3
 
 
+import argparse
 import csv
 import re
 import sys
 
 
 def main():
-    filename = sys.argv[1]
-    data = getDataIfFileExists(filename)
-    data = removePageHeaders(data)
-    print(data)
-    # try:
-    #     with open(filename, "rU") as f:
-    #         data = [line.rstrip().upper() for line in f]
-    #     data = [line for line in data if line is not ""]
-    #     data = [" ".join(line.split()) for line in data]
-    #     print(data)
-    #     # games = splitIntoGames(data)
-    #     # lines = []
-    #     # for game in games:
-    #     #     lines.append(getGameData(game))
-
-    #     # newname = filename.split(".")[0]
-    #     # header = ["team0name", "team0scores", "team1name", "team1scores"]
-    #     # with open("{}.csv".format(newname), "w") as f:
-    #     #     writer = csv.writer(f, delimiter=",")
-    #     #     writer.writerow(header)
-    #     #     for line in lines:
-    #     #         writer.writerow(line)
-    # except FileNotFoundError:
-    #     print("File {} not found".format(sys.argv[1]))
-    #     sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--infile', type=str)
+    parser.add_argument('-o', '--outfile', type=str)
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    args = parser.parse_args()
+    print(args)
+    data = get_data_from(args.infile)
+    game_data = save_box_scores(data)
+    print(game_data)
 
 
-def getDataIfFileExists(filename):
-    try:
-        with open(filename, "rU") as f:
-            data = [" ".join(line.upper().split()) for line in f]
-            data = [line for line in data if line is not ""]
-        return data
-    except:
-        print("File {} not found".format(filename))
-        sys.exit(1)
+def get_data_from(filename):
+    with open(filename, 'rU') as f:
+        data = [line.strip() for line in f if line.strip() != '']
+    draw_start = 'SCORING AND PERCENTAGES SUMMARY FOR DRAW 1'
+    index = data.index(draw_start)
+    return data[index:]
 
 
-def removePageHeaders(data):
-    basicHeader = data[:2]
-    newData = [entry for entry in data if entry not in basicHeader]
-    return newData
-
-
-def splitIntoGames(data):
-    games = []
+def save_box_scores(data):
+    box_scores = []
     game = []
+    box_match = re.compile('\d +\d +\d +\d')
     for line in data:
-        if line.startswith("*") or line.startswith("@"):
-            pass
-        else:
-            game.append(line)
-        if line.startswith("TEAM"):
-            games.append(game)
+        matches = re.findall(box_match, line)
+        if matches:
+            game.append(line.strip())
+        if len(game) == 3:
+            box_scores.append(game[1:])
             game = []
-    return games
-
-
-def getGameData(data):
-    if data[0].startswith("SCOR"):
-        data = data[2:]
-    elif data[0] == "":
-        data = data[3:]
-    else:
-        data = data[1:]
-    data[0] = " ".join(data[0].split()[1:-1])
-    data[1] = " ".join(data[1].split()[:-1])
-    newData = {}
-    teamName = re.compile(".+\)")
-    scores = re.compile("\*\d.+|\d.+")
-    for i, team in enumerate(("team0", "team1")):
-        newData[team] = {}
-        newData[team]["name"] = re.findall(teamName, data[i])[0]
-        newData[team]["scores"] = re.findall(scores, data[i])[0]
-    alllines = []
-    for team in newData:
-        line = []
-        line.append(newData[team]["name"])
-        line.append(newData[team]["scores"])
-        alllines.append(line)
-    for i in range(0, len(alllines), 2):
-        line = []
-        line.extend(alllines[i])
-        line.extend(alllines[i + 1])
-    return line
+    return box_scores
 
 
 if __name__ == "__main__":
