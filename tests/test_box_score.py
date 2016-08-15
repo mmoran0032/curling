23676fc1f70f0857c681591ef9c025d68d8e40d1
Mike Moran <mmoran0032@gmail.com>
2016-08-15 16:56:39 -0400

    Messed up which `test_box_score.py` was saved...

--- tests/test_box_score.py
+++ tests/test_box_score.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
 
 
 import unittest
@@ -7,7 +7,18 @@ from bs4 import BeautifulSoup
 
 from wcf import BoxScore
 
+with open('./tests/test_game.html', 'r') as f:
+    test_game_text = f.read()
+test_game = BeautifulSoup(test_game_text, 'html.parser')
+
 
 class TestBoxScore(unittest.TestCase):
     def setUp(self):
-        b = BoxScore()
+        self.b = BoxScore(test_game)
+
+    def test_game_loaded(self):
+        self.assertNotEqual(len(str(self.b)), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
